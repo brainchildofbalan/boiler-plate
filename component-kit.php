760e005c20777e32e4c9839bd9fdf45767f64eea
Aneesh <brainchildofbalan@gmail.com>
2023-06-26 22:36:07 +0530

    menu styling side bar

--- component-kit.php
+++ component-kit.php
@@ -17,70 +17,73 @@
 <body class="style-kit">
 
     <header>
-        <div class="header-inner">
-            <div class="logo"></div>
-        </div>
-        <div class="menu-links">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="/" class=" btn w-100 text-start">Home</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="./component-kit.php#color-section" class="btn w-100 text-start">Colors</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="./component-kit.php#image-ratio-section" class="btn w-100 text-start">Image Ratios</a>
-                        </li>
+        <div class="header_wrapper">
+            <div class="header-inner">
+                <div class="logo"></div>
+            </div>
+            <div class="menu-links">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="/" class=" btn w-100 text-start">Home</a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="./component-kit.php#color-section" class="btn w-100 text-start">Colors</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./component-kit.php#image-ratio-section" class="btn w-100 text-start">Image Ratios</a>
+                            </li>
 
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class=" btn w-100 text-start">Components</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Button</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Button Group</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">links</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Modal</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Forms</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class=" btn w-100 text-start">Types</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Paragraphs</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Titles</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a href="/" class=" btn w-100 text-start">Spacing</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Margin</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="/" class="btn w-100 text-start">Padding</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- <li class="nav-item">
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class=" btn w-100 text-start">Components</a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Button</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Button Group</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">links</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Modal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Forms</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class=" btn w-100 text-start">Types</a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Paragraphs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Titles</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/" class=" btn w-100 text-start">Spacing</a>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Margin</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/" class="btn w-100 text-start">Padding</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <!-- <li class="nav-item">
                     <a href="/" class=" btn w-100 text-start">Home</a>
                 </li> -->
 
-            </ul>
+                </ul>
+            </div>
+
         </div>
     </header>
 
@@ -109,30 +112,7 @@
                         <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
                         <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
                         <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
 
-
-                    </div>
-
-                    <div class="list-items d-flex flex-wrap">
-
-                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
-                        <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
-
-
-                    </div>
-
-
-                    <div class="list-items d-flex flex-wrap">
-
-                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
-                        <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
 
 
                     </div>
@@ -143,7 +123,7 @@
                         <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
                         <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
                         <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
+
 
 
                     </div>
@@ -155,7 +135,18 @@
                         <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
                         <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
                         <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
+
+
+
+                    </div>
+
+                    <div class="list-items d-flex flex-wrap">
+
+                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
+                        <div class="color-icon mt-3" style="background-color: #000;"></div>
+
 
 
                     </div>
@@ -167,18 +158,7 @@
                         <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
                         <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
                         <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
 
-
-                    </div>
-
-                    <div class="list-items d-flex flex-wrap">
-
-                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
-                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
-                        <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
 
 
                     </div>
@@ -190,7 +170,30 @@
                         <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
                         <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
                         <div class="color-icon mt-3" style="background-color: #000;"></div>
-                        
+
+
+
+                    </div>
+
+                    <div class="list-items d-flex flex-wrap">
+
+                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
+                        <div class="color-icon mt-3" style="background-color: #000;"></div>
+
+
+
+                    </div>
+
+
+                    <div class="list-items d-flex flex-wrap">
+
+                        <p class="mb-2 w-100 d-flex">color var : <b class="ms-auto d-inline-block">--primary</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color hex : <b class="ms-auto d-inline-block">#000</b><span class="copy">copy</span></p>
+                        <p class="mb-2 w-100 d-flex">color rgba : <b class="ms-auto d-inline-block">rgb(0 0 0)</b> <span class="copy">copy</span></p>
+                        <div class="color-icon mt-3" style="background-color: #000;"></div>
+
 
 
                     </div>
@@ -215,18 +218,18 @@
             </h3>
             <div class="w-100 d-flex  flex-column">
                 <div class="list row row-cols-4">
-                    
+
                     <div class="list-items d-flex flex-column">
                         <p class="mb-2 w-100 d-flex">ratio 1 x 1 <span class="copy">copy</span></p>
                         <div class="ratio-1x1 image-ratio">
-                            
+
                         </div>
                     </div>
 
                     <div class="list-items d-flex flex-column">
                         <p class="mb-2 w-100 d-flex">ratio 3 x 2 <span class="copy">copy</span></p>
                         <div class="ratio-3x2 image-ratio">
-                            
+
                         </div>
                     </div>
 
@@ -234,7 +237,7 @@
                     <div class="list-items d-flex flex-column">
                         <p class="mb-2 w-100 d-flex">ratio 5 x 4 <span class="copy">copy</span></p>
                         <div class="ratio-5x4 image-ratio">
-                            
+
                         </div>
                     </div>
 
@@ -242,7 +245,7 @@
                     <div class="list-items d-flex flex-column">
                         <p class="mb-2 w-100 d-flex">ratio 16 x 9 <span class="copy">copy</span></p>
                         <div class="ratio-16x9 image-ratio">
-                            
+
                         </div>
                     </div>
 
@@ -255,14 +258,14 @@
                     </div> -->
 
 
-                    
+
                 </div>
             </div>
 
 
         </div>
 
-        
+
         <!-- image ratio end -->
 
         <!-- button start -->
@@ -275,21 +278,21 @@
             </h3>
             <div class="w-100 d-flex  flex-column">
                 <div class="list row row-cols-auto g-2">
-                    
+
                     <div class="cols d-flex flex-wrap gap-3">
                         <button type="button" class="btn btn-primary">Primary</button>
-                    <button type="button" class="btn btn-secondary">Secondary</button>
-                    <button type="button" class="btn btn-success">Success</button>
-                    <button type="button" class="btn btn-danger">Danger</button>
-                    <button type="button" class="btn btn-warning">Warning</button>
-                    <button type="button" class="btn btn-info">Info</button>
-                    <button type="button" class="btn btn-light">Light</button>
-                    <button type="button" class="btn btn-dark">Dark</button>
+                        <button type="button" class="btn btn-secondary">Secondary</button>
+                        <button type="button" class="btn btn-success">Success</button>
+                        <button type="button" class="btn btn-danger">Danger</button>
+                        <button type="button" class="btn btn-warning">Warning</button>
+                        <button type="button" class="btn btn-info">Info</button>
+                        <button type="button" class="btn btn-light">Light</button>
+                        <button type="button" class="btn btn-dark">Dark</button>
 
-                    <button type="button" class="btn btn-link">Link</button>
+                        <button type="button" class="btn btn-link">Link</button>
 
                     </div>
-                    
+
                 </div>
             </div>
 
@@ -303,25 +306,25 @@
 
 
 
-         <!-- button group start -->
+        <!-- button group start -->
 
 
-         <div class="image-ratio-section py-5" id="image-ratio-section">
+        <div class="image-ratio-section py-5" id="image-ratio-section">
 
             <h3 class="mb-3">
                 Buttons
             </h3>
             <div class="w-100 d-flex  flex-column">
                 <div class="list row row-cols-auto g-2">
-                    
+
                     <div class="cols d-flex flex-wrap gap-3">
                         <div class="btn-group" role="group" aria-label="Basic example">
                             <button type="button" class="btn btn-primary">Left</button>
                             <button type="button" class="btn btn-primary">Middle</button>
                             <button type="button" class="btn btn-primary">Right</button>
-                          </div>
+                        </div>
                     </div>
-                    
+
                 </div>
             </div>
 
